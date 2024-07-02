2a1ba836b3cd6024cc6025620fdafa844e31075d
Phylumium <phylumium3000@gmail.com>
2024-07-02 10:12:17 +0000

    added variable success

--- sum_of_natural_nums.py
+++ sum_of_natural_nums.py
@@ -1,8 +1,10 @@
-def sum_of_natural_nums(num):
+def sum_of_natural_nums(num,sum):
     # Base Case 
     if num == 0:
-        return 0
+        return sum
     else:
-        return num + sum_of_natural_nums(num - 1)
+        sum += num
+        return sum_of_natural_nums(num-1,sum)
+        # return num + sum_of_natural_nums(num - 1)
 
-print(sum_of_natural_nums(10))
+print(sum_of_natural_nums(10,0))
